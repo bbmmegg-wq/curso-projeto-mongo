a4199edb57032d679f1e4534bad60969b4eb7469
Beatriz Moreira <bbmmegg@gmail.com>
2026-02-04 19:19:20 -0300

    Referenciando os posts do  usuario

--- workshopmongo/src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
+++ workshopmongo/src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
@@ -15,35 +15,39 @@ import com.nelioalves.workshopmongo.dto.AuthorDTO;
 import com.nelioalves.workshopmongo.repository.PostRepository;
 import com.nelioalves.workshopmongo.repository.UserRepository;
 
-
 @Configuration
 public class Instantiation implements CommandLineRunner {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 		ZoneId zone = ZoneId.of("GMT");
-		
+
 		userRepository.deleteAll();
 		postRepository.deleteAll();
-		
-		User maria = new User(null, "Maria Brown", "maria@gmail.com"); 
-		User alex = new User(null, "Alex Green", "alex@gmail.com"); 
+
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
+
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
-		
-		Post post1 = new Post(null, LocalDateTime.parse("21/03/2018 00:00", fmt) .atZone(zone).toInstant(), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
-		Post post2 = new Post(null, LocalDateTime.parse("23/03/2018 00:00", fmt) .atZone(zone).toInstant(), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
-		
+
+		Post post1 = new Post(null, LocalDateTime.parse("21/03/2018 00:00", fmt).atZone(zone).toInstant(),
+				"Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, LocalDateTime.parse("23/03/2018 00:00", fmt).atZone(zone).toInstant(), "Bom dia",
+				"Acordei feliz hoje!", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
+
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 }
