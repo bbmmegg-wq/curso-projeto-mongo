f735620787f08ca810a78d422f1af03d53d0622a
Beatriz Moreira <bbmmegg@gmail.com>
2026-02-04 21:11:06 -0300

    Consulta simples com Query methods

--- workshopmongo/src/main/java/com/nelioalves/workshopmongo/services/PostService.java
+++ workshopmongo/src/main/java/com/nelioalves/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.nelioalves.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,10 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
-
+	public List<Post> findByTitle (String text){
+		
+		return repository.findByTitleContainingIgnoreCase(text);
+	}
 	
 	
 }
